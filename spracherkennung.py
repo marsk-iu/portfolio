1bbaaaca7e0f6893c74a7a419bf0f66fdecee2b9
Marsk <marian.hilpert@mailbox.org>
2024-06-29 10:10:03 +0200

    csv step added

--- spracherkennung.py
+++ spracherkennung.py
@@ -2,18 +2,21 @@ import cld3 #pycld3 zur Spracherkennung
 import pandas as pd
 
 #pandas DataFrame erstellen mit csv als Eingabe
-data = pd.read_csv('subset.csv') 
+data = pd.read_csv('Bewertungen_nobreaks.csv') 
 
 #Spalte mit Sprache hinzufügen
 data['Sprache'] = "" 
 
 #Sprache für jede Bewertung erkennen und entsprechend eintragen
 for i in data.index: 
-    data['Sprache'].iloc[i] = cld3.get_language(data.iloc[i]['Bewertungen']).language
-
+    data.loc[i, 'Sprache'] = cld3.get_language(data.iloc[i]['Bewertungen']).language
+    
 #neues Dataframe erstellen, welches nur jenen Bewertungen enthält, die als Deutsch erkannt wurden    
 corpus = data[data['Sprache']=="de"]['Bewertungen']
 
+#Dataframe als .csv speichern
+corpus.to_csv('Bewertungen_de.csv', sep=',', index=False)
+
 #zur Kontrolle
-print(data)
-print(corpus)
+#print(data)
+#print(corpus)
